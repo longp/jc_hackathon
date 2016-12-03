25ef6dd5db72bedc932aeab2235c9f9f60929a82
Jeffrey Shapiro <jeffreyhshapiro@gmail.com>
2016-12-03 13:21:29 -0500

    api calls and stuff

--- api/api.js
+++ api/api.js
@@ -1,24 +1,37 @@
 var express = require('express');
 var app = express();
-require('dotenv').config();
+var dotenv = require('dotenv').config({ path: '.env' });
 var PORT = 3000;
 
-dotenv.load({ path: '.env' });
 var gcloud = require('google-cloud');
-var vision = gcloud.vision;
-
 var vision = require('@google-cloud/vision');
 var visionClient = vision({
   projectId: process.env.GCLOUD_PROJECT,
   keyFilename: process.env.FILE_PATH
 });
 
-
-visionClient.detectText('./IMG_2018.jpg', function(err, text) {
-  console.log(err)
-  console.log(text)
+var language = require('@google-cloud/language');
+var languageClient = language({
+  projectId: process.env.GCLOUD_PROJECT,
+  keyFilename: process.env.FILE_PATH
 });
 
+var imageText = [];
+
+visionClient.detectText('IMG_2132.jpg', function(err, text) {
+  if (err) {
+    throw err
+  } else {
+    var text = text.join(" ");
+    languageClient.detectEntities(text, function(err, entities) {
+      if (err) {
+        throw err
+      } else {
+        console.log(entities);
+      }
+    });
+  }
+});
 
 app.listen(PORT, function() {
   console.log('listening on ' + PORT);
